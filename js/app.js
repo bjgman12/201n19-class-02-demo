2446c9ed02dc77235f1b8107e8ffa4477a66b4b3
Brandon Gonzalez <bjgman38@gmail.com>
2020-07-22 21:54:33 -0600

    finished demo

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function Question(){
+    var answerOne = confirm('Are you ready to rumble');
+
+    if (answerOne == true) {
+        console.log('Let\s get ready to rumble!!')
+    } else  {
+        console.log('Then go home!!')
+    }
+}
+
